1b5ff7847e871e449c5ce29d36623b9919eda289
JIHYE <jihyem75@gmail.com>
2023-08-07 16:01:56 +0900

    :lipstick: design: create left navbar

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,44 @@ class MyAppState extends ChangeNotifier {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {  // 모든 위젯은 위젯이 항상 최신 상태로 유지되도록 위젯의 상황이 변경될 때마다 자동으로 호출되는 `build()` 메서드를 정의
-    var appState = context.watch<MyAppState>(); // `MyHomePage` 는 `watch` 메서드를 사용하여 앱의 현재 상태에 관한 변경사항을 추적
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+              child: NavigationRail(
+                extended: false,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: 0,
+                onDestinationSelected: (value) {
+                  print('selected: $value');
+                },
+              ),
+          ),
+          Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: GeneratorPage(),
+              ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
     IconData icon;
@@ -58,36 +95,32 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(  // 모든 `build` 메서드는 위젯 또는 중첩된 위젯 트리(좀 더 일반적임)를 반환해야합. 여기서 최상위 위젯은 `Scaffold`. 유용한 위젯이며 대부분의 실제 Flutter 앱에서 찾을 수 있음
-      body: Center(
-        child: Column( // Flutter 에서 가장 기본적인 레이아웃 위젯 중 하나. 하위 요소를 원하는 대로 사용하고 이를 위에서 아래로 열에 배치함. 기본적으로 열은 시각적으로 하위 요소를 상단에 배치함
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-
-                ElevatedButton.icon(
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
                   onPressed: () {
-                  appState.toggleFavorite();
+                    appState.toggleFavorite();
                   },
                   icon: Icon(icon),
                   label: Text('Like'),
-                ),
-                SizedBox(width: 10),
-
-                ElevatedButton(
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
                   onPressed: () {
                     appState.getNext();
                   },
                   child: Text('Next'),
-                ),
-              ],
-            ),
-          ],  // Flutter 코드에서는 후행 쉼표를 많이 사용함. 이 특정 쉼표는 여기 없어도 됨. `children` 이 이 특정 `Column` 매개변수 목록의 마지막 멤버이자 유일한 멤버이기 때문. 그러나 일반적으로 후행 쉼표를 사용하는 것이 좋음. 멤버를 더 추가하는 작업이 쉬워지고, Dart 의 자동 형식 지정 도구에서 줄바꿈을 추가하도록 힌트 역할을 함
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
